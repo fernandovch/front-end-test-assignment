2e54738a4de7328d44e63aaebd3af767cc34654d
Fernando Valverde <fernando.valverde@cision.com>
2022-04-22 01:50:52 -0600

    centering search box

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,22 +21,35 @@ function App() {
 const currencyData : Currency[] = props
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header className='header-section'>
+
+      </header>
+      <section className="middle-section">
          <div>
          <CurrencyList currencyData={ currencyData } />
          </div>
          <div className="image-center">
            &nbsp;
          </div>
-         <div className="searchBox"></div>         
-      </header>
+         <div className="image-search">
+
+            <div className='search-box'>              
+              <input></input>
+              <button></button>
+              <p></p>
+            </div>
+            
+
+           </div>         
+      </section>     
       <footer className='footer'>
         <section>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam feugiat orci at egestas commodo. Fusce ac massa eu leo varius consectetur at non enim. Etiam aliquet et dolor vitae viverra. Phasellus maximus justo nec tincidunt ullamcorper. In semper, odio eget rutrum bibendum, odio metus blandit neque, non lobortis lorem nulla eget lectus. Vivamus vel congue augue, iaculis sagittis odio. Praesent sit amet facilisis felis, sit amet dignissim justo. Proin ac dignissim nulla. Cras ultrices diam felis, non placerat tortor rutrum non.
         </section>
       </footer>
     </div>
+    
   );
 }
 
